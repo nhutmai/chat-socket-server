f2dfad9adb976abbcd3ca3fe2ff2fb2b32aaa695
nhutmai <nhutmai00@gmail.com>
2025-09-19 11:01:38 +0700

    fix

--- src/utils/constant.ts
+++ src/utils/constant.ts
@@ -1,6 +1,6 @@
 const redisHost = process.env.REDIS_HOST || 'localhost';
 const redisPort = parseInt(process.env.REDIS_PORT || '6379', 0);
-const webPort = process.env.WEB_PORT || 3000;
+const webPort = process.env.PORT || 3000;
 
 const redisChannel = {
   inRoomMessage: 'in-room-message',
